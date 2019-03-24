f91f44b92b10a9dd733d240cbeb2e94ec56f7379
J. Singleton <jennifer.singleton@gmail.com>
2019-03-24 17:36:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,13 +4,13 @@ class Application
     resp = Rack::Response.new
 
     noon = Time.new(2019, 1, 1, 12, 0, 0)
-
+binding.pry
     if Time.now < noon
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
     end
-    
+
     resp.finish
   end
 
